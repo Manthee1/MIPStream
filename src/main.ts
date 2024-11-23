0e08955b1597bcd867d137ca4a4f515998e282fc
Manthee1 <dinja.martin@gmail.com>
2024-11-23 11:13:05 +0100

    Integrate view and settings stores into main application setup

--- src/main.ts
+++ src/main.ts
@@ -3,40 +3,48 @@ import { createApp } from "vue";
 
 import { createPinia } from 'pinia'
 import { useDlxStore } from './stores/dlxStore'
+import { useViewStore } from './stores/viewStore'
+import { useSettingsStore } from './stores/settingsStore'
+import VueFeather from 'vue-feather';
 
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from './routes.ts'
 
-import VueFeather from 'vue-feather';
-
-
-
 const pinia = createPinia()
-
-const app = createApp(App);
-
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
 })
 
+const app = createApp(App);
+
+
 app.use(pinia);
+
+app.use(router);
+
+app.component(VueFeather.name ?? '', VueFeather);
+
+// Stores
 const dlxStore = useDlxStore();
+const viewStore = useViewStore();
+const settings = useSettingsStore();
 
 declare module '@vue/runtime-core' {
     interface ComponentCustomProperties {
         $dlxStore: ReturnType<typeof useDlxStore>
+        $viewStore: ReturnType<typeof useViewStore>
+        $settings: ReturnType<typeof useSettingsStore>
     }
 }
 // @ts-ignore
 window.dlxStore = dlxStore;
 
 app.config.globalProperties.$dlxStore = dlxStore;
+app.config.globalProperties.$viewStore = viewStore;
+app.config.globalProperties.$settings = settings;
 
-app.use(router);
-
-app.component(VueFeather.name ?? '', VueFeather);
 
 app.mount("#app");
 
