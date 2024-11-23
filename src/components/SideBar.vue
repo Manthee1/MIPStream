461ae8d0fe13fd81d145ea8627c3ed8fa24f8ee2
Manthee1 <dinja.martin@gmail.com>
2024-11-23 11:16:40 +0100

    Add SideBar component with Accordion sections for Stages, Registers, and Memory

---
+++ src/components/SideBar.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="sidebar">
+        <Accordion label="Stages" icon="layers">
+            <Stages />
+        </Accordion>
+        <Accordion label="Registers" icon="hard-drive">
+            <Registers />
+        </Accordion>
+        <Accordion label="Memory" icon="database">
+            <Memory />
+        </Accordion>
+    </div>
+</template>
+
+<script>
+import Accordion from './Accordion.vue';
+import Memory from './Memory.vue';
+import Registers from './Registers.vue';
+import Stages from './Stages.vue';
+export default {
+    components: { Accordion, Stages, Registers, Memory },
+    name: 'SideBar',
+
+};
+</script>
+
+<style scoped>
+.sidebar {
+    background-color: var(--color-background-dark);
+    height: 100%;
+    width: 300px;
+    min-width: 300px;
+    overflow: hidden;
+}
+</style>
