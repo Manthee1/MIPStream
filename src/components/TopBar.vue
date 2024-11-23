0d0a181c29f779c82a9e720614d03bfcc8906c70
Manthee1 <dinja.martin@gmail.com>
2024-11-23 11:14:46 +0100

    Added TopBar component with dropdown and theme toggle functionality

---
+++ src/components/TopBar.vue
@@ -0,0 +1,113 @@
+<template>
+    <div class="top-bar">
+        <Dropdown compact :items="dropdownItems" label="" icon="menu" />
+        <span class="file-name">DLXSim - {{ fileName }}</span>
+        <!-- Theme -->
+        <div class="flex">
+            <MButton :icon="$viewStore.theme === 'light' ? 'sun' : 'moon'" @click="$viewStore.toggleTheme" />
+            <MButton icon="cpu" @click="toggleView" class="toggle-view" :class="{ active: showCpuView }" />
+        </div>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import Dropdown from './Dropdown.vue';
+import MButton from './MButton.vue';
+export default defineComponent({
+    name: 'TopBar',
+    components: {
+        MButton,
+        Dropdown
+    },
+    data() {
+        return {
+            dropdownItems: [
+                {
+                    label: 'New',
+                    action: () => {
+                        console.log('New clicked');
+                    }
+                },
+                {
+                    label: 'Open',
+                    action: () => {
+                        console.log('Open clicked');
+                    }
+                },
+                {
+                    label: 'Save',
+                    action: () => {
+                        console.log('Save clicked');
+                    }
+                },
+                {
+                    label: 'Save As',
+                    action: () => {
+                        console.log('Save As clicked');
+                    }
+                },
+                {
+                    label: 'Settings',
+                    action: () => {
+                        this.$viewStore.toggleSettings();
+                    }
+                },
+                {
+                    label: 'Help',
+                    action: () => {
+                        console.log('Help clicked');
+                    }
+                },
+                {
+                    label: 'About',
+                    action: () => {
+                        console.log('About clicked');
+                    }
+                }
+            ]
+        }
+    },
+    props: {
+        fileName: {
+            type: String,
+            required: true
+        }
+    },
+    computed: {
+        showCpuView() {
+            return this.$viewStore.showCpuView;
+        }
+    },
+    methods: {
+        toggleView() {
+            this.$viewStore.toggleCpuView();
+        }
+    }
+})
+</script>
+
+<style lang="sass" scoped>
+.top-bar 
+    display: flex
+    justify-content: space-between
+    align-items: center
+    background-color: var(--color-background)
+    color: var(--color-regular)
+    padding: 0em 0.4em
+
+    .hamburger-menu,
+    .toggle-view 
+        background: none
+        border: none
+        color: var(--color-regular)
+        font-size: 20px
+        cursor: pointer
+        &.active
+            background-color: var(--color-light)
+            border-radius: 1em
+
+    .file-name 
+        font-size: 16px
+
+</style>
