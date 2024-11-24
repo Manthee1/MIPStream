d6d369779a1c686dccb15a514150463384f8d9a9
Manthee1 <dinja.martin@gmail.com>
2024-11-24 19:24:54 +0100

    Add Switch component with toggle functionality and styles

---
+++ src/components/common/Switch.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="switch" :class="{ 'switch--on': modelValue }" @click="toggle">
+        <div class="switch__toggle"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Switch',
+    props: {
+        modelValue: {
+            type: Boolean,
+            default: false
+        }
+    },
+    methods: {
+        toggle(e) {
+            this.$emit('update:modelValue', !this.modelValue);
+        }
+    }
+};
+</script>
+
+<style scoped>
+.switch {
+    width: 50px;
+    height: 25px;
+    background-color: var(--color-medium);
+    border-radius: 25px;
+    position: relative;
+    cursor: pointer;
+    transition: background-color 0.3s;
+}
+
+.switch--on {
+    background-color: var(--color-accent);
+}
+
+.switch__toggle {
+    width: 23px;
+    height: 23px;
+    background-color: var(--color-background);
+    border-radius: 50%;
+    position: absolute;
+    top: 1px;
+    left: 1px;
+    transition: left 0.3s;
+}
+
+.switch--on .switch__toggle {
+    left: 26px;
+}
+</style>
