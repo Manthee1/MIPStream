3aa684f08d758833d8b32740c2157f5f1d8266d4
Manthee1 <dinja.martin@gmail.com>
2024-11-26 21:04:54 +0100

    Enhance validation logic in Monaco editor for assembly instructions, adding checks for register usage and operand count

--- src/config/monaco.ts
+++ src/config/monaco.ts
@@ -67,7 +67,62 @@ export function validate(model: monaco.editor.ITextModel) {
                 });
                 return;
             }
+
+            if (instruction.mnemonic === 'HALT' || instruction.mnemonic === 'NOP') return;
+
+            const registerCount = line.match(registerRegex)?.length ?? 0;
+
+            // If the the first register is R0, warn the user
+            if (instruction.type !== InstructionType.J && line.trim().replace(',', ' ').split(' ')[1]?.trim() === 'R0') {
+                errors.push({
+                    startLineNumber: index + 1,
+                    startColumn: line.indexOf('R0') + 1,
+                    endLineNumber: index + 1,
+                    endColumn: line.indexOf('R0') + 3,
+                    message: 'R0 should not be used as a destination register as it is hardwired to zero and will not be modified',
+                    severity: monaco.MarkerSeverity.Warning,
+                });
+            }
+
+            if (instruction.type === InstructionType.R) {
+                if (registerCount !== 3) {
+                    errors.push({
+                        startLineNumber: index + 1,
+                        startColumn: 1,
+                        endLineNumber: index + 1,
+                        endColumn: line.length,
+                        message: `Expected 3 registers, found ${registerCount}`,
+                        severity: monaco.MarkerSeverity.Error,
+                    });
+                }
+            } else if (instruction.type === InstructionType.I) {
+                if (registerCount !== 2) {
+                    errors.push({
+                        startLineNumber: index + 1,
+                        startColumn: 1,
+                        endLineNumber: index + 1,
+                        endColumn: line.length,
+                        message: `Expected 2 registers, found ${registerCount}`,
+                        severity: monaco.MarkerSeverity.Error,
+                    });
+                }
+            }
+
+            if (instruction.memOp === MemOp.LOAD || instruction.memOp === MemOp.STORE) {
+                if (line.trim().split(',').length !== 2) {
+                    errors.push({
+                        startLineNumber: index + 1,
+                        startColumn: 1,
+                        endLineNumber: index + 1,
+                        endColumn: line.length,
+                        message: `Expected 2 operands, found ${line.trim().split(',').length}`,
+                        severity: monaco.MarkerSeverity.Error,
+                    });
+                }
+            }
         }
+
+
     });
 
     monaco.editor.setModelMarkers(model, 'asm', errors);
