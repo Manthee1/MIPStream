ac4390427d9044b3f87055a85dcf56c5b8351edf
Manthee1 <dinja.martin@gmail.com>
2024-11-26 23:40:59 +0100

    Add hover provider for Monaco editor; implement instruction and register hover information for enhanced code assistance

---
+++ src/config/monaco/hoverProvider.ts
@@ -0,0 +1,55 @@
+import * as monaco from 'monaco-editor';
+// import * as themeData from 'monaco-themes/themes/Dawn.json';
+import INSTRUCTION_SET from '../../assets/js/config/instructionSet';
+import { InstructionDef, InstructionType, MemOp } from '../../assets/js/interfaces/instruction';
+import { getInstructionSyntax } from '../../assets/js/utils';
+import { useViewStore } from '../../stores/viewStore';
+
+// Constants
+const mnemonics = INSTRUCTION_SET.map((instruction) => instruction.mnemonic);
+const registers = Array.from({ length: 32 }, (_, i) => `R${i}`);
+const mnemonicRegex = new RegExp(`\\b(${mnemonics.join('|')})\\b`, 'g');
+const registerRegex = new RegExp(`\\b(${registers.join('|')})\\b`, 'g');
+
+export default {
+    provideHover: function (model, position) {
+        const word = model.getWordAtPosition(position);
+        if (word) {
+            const { word: text } = word;
+            if (mnemonicRegex.test(text)) {
+                const instruction = INSTRUCTION_SET.find(inst => inst.mnemonic === text);
+                if (instruction) {
+                    setTimeout(() => {
+                        // Find the data-href attribute of the first link
+                        const link = document.querySelector(`a[data-href*="${instruction.mnemonic}--docs"]`);
+                        if (!link) return;
+                        link.addEventListener('click', (e) => {
+                            console.log('clicked');
+                            // useViewStore().showInstructionDocs(instruction);
+                        });
+                    }, 1000);
+
+                    return {
+                        range: new monaco.Range(position.lineNumber, word.startColumn, position.lineNumber, word.endColumn),
+                        contents: [
+                            {
+                                value: `*(Instruction)* [**${instruction.mnemonic}**](${instruction.mnemonic}--docs) <br> ${instruction.description}`,
+                                supportThemeIcons: true,
+                                supportHtml: true
+                            }
+                        ]
+                    };
+                }
+            } else if (registerRegex.test(text)) {
+                return {
+                    range: new monaco.Range(position.lineNumber, word.startColumn, position.lineNumber, word.endColumn),
+                    contents: [
+                        { value: `** Register ${text} ** ` },
+                        { value: 'General purpose register' }
+                    ]
+                };
+            }
+        }
+        return null;
+    }
+} as monaco.languages.HoverProvider;
