92e85e64ac8f8315640372f37db5c55538481884
Manthee1 <dinja.martin@gmail.com>
2024-11-27 19:45:47 +0100

    Add Problems component to display error messages from the store

---
+++ src/components/core/Problems.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="problems-container">
+        <template v-if="this.$dlxStore.errors.length > 0">
+            <ul class="error-list">
+                <li class='error-item flex flex-left' v-for="error in this.$dlxStore.errors" :key="error">
+                    <vue-feather class="icon" type="alert-triangle" />
+                    <span class="my-auto">{{ error }}</span>
+                </li>
+            </ul>
+        </template>
+        <p v-else>No errors</p>
+    
+    
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="sass" scoped>
+.problems-container
+    padding: 2rem
+    min-height: 200px
+    .error-list
+        list-style-type: none
+        padding: 0
+        margin: 0
+    .error-item
+        background-color: var(--color-background)
+        color: var(--color-system-error)
+        border: 1px solid var(--color-background)
+        border-radius: 5px
+        overflow: hidden
+        text-overflow: ellipsis
+        .icon
+            margin-right: 5px
+            height: 16px
+</style>
