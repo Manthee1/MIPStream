ff4633406cafdc864d6a41af6e94542ac517a8e8
Manthee1 <dinja.martin@gmail.com>
2024-11-28 14:36:20 +0100

    Refactor Controls component: streamline button rendering and add resume method

--- src/components/core/Controls.vue
+++ src/components/core/Controls.vue
@@ -5,10 +5,8 @@
 <template>
 	<div class="controls">
 		<MButton v-if="$dlxStore.status == 'stopped'" outlined small icon="play" @click="run" title="Run"></MButton>
-		<MButton v-else-if="$dlxStore.status == 'running'" outlined small icon="pause" @click="pause" title="Pause">
-		</MButton>
-		<MButton v-else-if="$dlxStore.status == 'paused'" outlined small icon="play-circle" @click="run" title="Resume">
-		</MButton>
+		<MButton v-else-if="$dlxStore.status == 'running'" outlined small icon="pause" @click="pause" title="Pause" />
+		<MButton v-else-if="$dlxStore.status == 'paused'" outlined small icon="play-circle" @click="resume" title="Resume" />
 		<MButton outlined small icon="square" @click="stop" title="Stop"></MButton>
 		<MButton outlined small icon="skip-forward" @click="step" title="Step"></MButton>
 		<MButton outlined small icon="refresh-cw" @click="reloadProgram" title="Reload Program"></MButton>
@@ -21,8 +19,6 @@ import { defineComponent } from "vue";
 import MButton from "@/components/common/MButton.vue";
 
 
-
-
 export default defineComponent({
 
 	name: 'Controls',
@@ -30,30 +26,23 @@ export default defineComponent({
 		MButton
 	},
 	methods: {
+		resume() {
+			this.$dlxStore.resume();
+		},
 		run() {
-			console.log('Run clicked');
 			this.$dlxStore.run();
-			// Add your run logic here
 		},
 		pause() {
-			console.log('Pause clicked');
 			this.$dlxStore.pause();
-			// Add your pause logic here
 		},
 		stop() {
-			console.log('Stop clicked');
 			this.$dlxStore.stop();
-			// Add your stop logic here
 		},
 		step() {
-			console.log('Step clicked');
 			this.$dlxStore.step();
-			// Add your step logic here
 		},
 		reloadProgram() {
-			console.log('Reload Program clicked');
 			this.$dlxStore.loadProgram();
-			// Add your reload program logic here
 		}
 	}
 });
