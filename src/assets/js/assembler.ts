7cbdd653915c34d6f55b47635553cea2343bfa81
Manthee1 <dinja.martin@gmail.com>
2024-12-03 18:04:07 +0100

    Refactor instruction encoding by adding operand extraction function and validating operand counts

--- src/assets/js/assembler.ts
+++ src/assets/js/assembler.ts
@@ -1,13 +1,19 @@
-import { ALUopcode, CPU, Memory } from "./interfaces/core"
+import { Memory } from "./interfaces/core"
 import { InstructionR, InstructionI, InstructionJ, InstructionType, MemOp, OperandType, OperandRole } from "./interfaces/instruction"
 import INSTRUCTION_SET from "./config/instructionSet"
-import { createBlankStageData, getEffectiveAddressImm, getEffectiveAddressRegister, getRegisterNumber, isEffectiveAddress, isIType, isJType, isLabel, isMnemonic, isRegister, isRType, isValidRegister, isValue, isXBit, } from "./utils"
+import { getEffectiveAddressImm, getEffectiveAddressRegister, getRegisterNumber, isEffectiveAddress, isIType, isJType, isLabel, isMnemonic, isRegister, isRType, isValidRegister, isValue, isXBit, } from "./utils"
 import { AssemblerError, AssemblerErrorList, ErrorType } from "./errors";
 
 
-export function encodeInstruction(instruction: string, labels: Map<string, number>): InstructionR | InstructionI | InstructionJ {
+function getOperands(instruction: string): string[] {
+    // split the string on a comma or space
+    return instruction.split(/,|\s/).slice(1).filter((operand) => operand !== '');
+}
+
+
+export function encodeInstruction(instruction: string, labels: Map<string, number>, pc: number): InstructionR | InstructionI | InstructionJ {
     const mnemonic = instruction.split(" ")[0];
-    const operands = instruction.split(" ").slice(1).join('').split(",").map((operand) => operand.trim());
+    const operands = getOperands(instruction);
     // If the instruction NOP, return a NOP instruction
     if (mnemonic === "NOP")
         return { opcode: 0, rs: 0, rd: 0, imm: 0 } as InstructionI;
@@ -18,7 +24,7 @@ export function encodeInstruction(instruction: string, labels: Map<string, numbe
 
     const instructionDef = INSTRUCTION_SET[instructionOpcode];
 
-    let encodedInstruction: InstructionR | InstructionI | InstructionJ;
+    let encodedInstruction: InstructionR | InstructionI | InstructionJ = {} as InstructionR | InstructionI | InstructionJ;
     if (!INSTRUCTION_SET[instructionOpcode].operands) throw new Error(`Instruction ${mnemonic} is not configured correctly.`);
 
 
@@ -35,11 +41,21 @@ export function encodeInstruction(instruction: string, labels: Map<string, numbe
         operands.push(imm.toString());
     }
 
+    // Check if the number of operands is correct
+    const expectedOperandCount = INSTRUCTION_SET[instructionOpcode].operands.filter((operand) => operand.type !== OperandType.UNUSED).length;
+    if (operands.length !== expectedOperandCount) {
+        throw new Error(`Invalid number of operands for instruction ${mnemonic}.`);
+    }
+
 
     let decodedValues = [0, 0, 0]
     let isFirstSourceOperand = true;
 
-    INSTRUCTION_SET[instructionOpcode].operands.forEach((operand, index) => {
+    console.log('operands', operands);
+
+
+    let index = 0;
+    INSTRUCTION_SET[instructionOpcode].operands.forEach((operand) => {
         if (operand.type === OperandType.UNUSED) return;
 
         let value = 0;
@@ -53,10 +69,16 @@ export function encodeInstruction(instruction: string, labels: Map<string, numbe
             if (isXBit(parseInt(operands[index]), 16)) throw new Error(`Immediate value out of range: ${operands[index]}.`);
             value = parseInt(operands[index]);
         } else if (operand.type === OperandType.LABEL) {
+            console.log('Label index', index);
+
             if (!isLabel(operands[index])) throw new Error(`Invalid label: ${operands[index]}.`);
             if (!labels.has(operands[index])) throw new Error(`Invalid label: ${operands[index]}.`);
             value = labels.get(operands[index]) as number;
+            value = value - pc - 3;
         } else throw new Error(`Invalid operand type: ${operand.type}.`);
+
+        index++;
+
 
         if (operand.role === OperandRole.DESTINATION) {
             decodedValues[0] = value;
@@ -72,9 +94,12 @@ export function encodeInstruction(instruction: string, labels: Map<string, numbe
             return;
         }
         if (operand.role === OperandRole.IMMEDIATE) {
-            decodedValues[3] = value;
+            decodedValues[2] = value;
         }
     });
+
+    console.log('Decoded values', decodedValues);
+
 
 
     switch (instructionDef.type) {
@@ -97,7 +122,7 @@ export function encodeInstruction(instruction: string, labels: Map<string, numbe
         case InstructionType.J:
             encodedInstruction = {
                 opcode: instructionOpcode,
-                address: decodedValues[2],
+                offset: decodedValues[0],
             } as InstructionJ;
             break;
     }
@@ -141,6 +166,7 @@ export function assemble(program: string): { memory: Memory, labels: Map<string,
 
     // Encode instructions
     let line = 0;
+    pc = 0;
     programLines.forEach((instruction) => {
         line++;
         if (instruction.trim() === '') return;
@@ -148,20 +174,19 @@ export function assemble(program: string): { memory: Memory, labels: Map<string,
         if (instruction.trim().endsWith(':')) return;
         let encodedInstruction;
         try {
-            encodedInstruction = encodeInstruction(instruction, labels);
+            encodedInstruction = encodeInstruction(instruction, labels, pc);
             encodedInstructions.push(encodedInstruction);
         } catch (error: any) {
             errors.push(new AssemblerError(ErrorType.SYNTAX_ERROR, line, error.message));
         }
-
-
+        pc++;
     });
     if (errors.length) {
         console.log('Errors', errors);
         throw errors;
     }
     // Add A HALT and 4 NOP instructions to the end of the program
-    encodedInstructions.push(encodeInstruction("HALT", labels) as InstructionJ);
+    encodedInstructions.push(encodeInstruction("HALT", labels, pc) as InstructionJ);
     encodedInstructions.push({ opcode: 0, rs: 0, rd: 0, imm: 0 } as InstructionI);
     encodedInstructions.push({ opcode: 0, rs: 0, rd: 0, imm: 0 } as InstructionI);
     encodedInstructions.push({ opcode: 0, rs: 0, rd: 0, imm: 0 } as InstructionI);
