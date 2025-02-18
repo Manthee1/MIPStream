deb24d88e419f1ddd17b2e738068293f11f3f756
Manthee1 <dinja.martin@gmail.com>
2025-02-18 11:33:48 +0100

    Refactor Adder component: improve code formatting and adjust input port positioning

--- src/assets/js/core/components/Adder.ts
+++ src/assets/js/core/components/Adder.ts
@@ -1,36 +1,39 @@
+/** @format */
+
 import { ComponentType } from "../../types/enums";
 import { ComponentBase } from "./ComponentBase";
 
 export class Adder extends ComponentBase {
-    id = 'Adder';
-    name = 'Adder';
-    type = ComponentType.Gate;
-    description = 'Adder';
-    controlInputs = [];
+	id = "Adder";
+	name = "Adder";
+	type = ComponentType.Gate;
+	description = "Adder";
+	controlInputs = [];
 
+	portsLayout: PortLayout[] = [
+		{
+			name: "out",
+			location: "right",
+			relPos: 0.5,
+		},
+	];
 
-    portsLayout: PortLayout[] = [
-        {
-            name: 'out',
-            location: 'right',
-            relPos: 0.5
-        }
-    ];
-
-    constructor(id: string, inputCount: number, bits: number) {
-        super();
-        this.id = id;
-        this.inputs = Array.from({ length: inputCount }, (_v, k) => ({ bits: bits, name: 'in' + (k + 1) }));
-        this.outputs = [{ bits: bits, name: 'out' }];
-        this.portsLayout = this.inputs.map((_, i) => {
-            return <PortLayout>{
-                name: 'in' + (i + 1),
-                location: 'left',
-                relPos: (i + 1) / (inputCount + 1),
-            };
-        }).concat(this.portsLayout);
-    }
-    public execute(inputs: Array<number>): Array<number> {
-        return [(inputs[0] + inputs[1]) | 0];
-    }
+	constructor(id: string, inputCount: number, bits: number) {
+		super();
+		this.id = id;
+		this.inputs = Array.from({ length: inputCount }, (_v, k) => ({ bits: bits, name: "in" + (k + 1) }));
+		this.outputs = [{ bits: bits, name: "out" }];
+		this.portsLayout = this.inputs
+			.map((_, i) => {
+				return <PortLayout>{
+					name: "in" + (i + 1),
+					location: "left",
+					relPos: (i + 0.5) / (inputCount + 0),
+				};
+			})
+			.concat(this.portsLayout);
+	}
+	public execute(inputs: Array<number>): Array<number> {
+		return [(inputs[0] + inputs[1]) | 0];
+	}
 }
