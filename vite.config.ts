a9bc3c2d33e545b1dc4078f4af26aefc0068eb92
Manthee1 <dinja.martin@gmail.com>
2025-05-01 21:42:58 +0200

    fix: update Vite configuration to include host and HMR settings

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import vue from "@vitejs/plugin-vue";
 import path from 'path';
 const __dirname = path.resolve();
 
+const host = process.env.TAURI_DEV_HOST;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '',
@@ -16,14 +18,29 @@ export default defineConfig({
   server: {
     port: 1420,
     strictPort: true,
+
+    host: host || false,
+    hmr: host
+      ? {
+        protocol: 'ws',
+        host,
+        port: 1421,
+      } : undefined,
+
     watch: {
       // 3. tell vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
     },
+  },
+  envPrefix: ['VITE_', 'TAURI_ENV_*'],
+  build: {
+    target: process.env.TAURI_ENV_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+    minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
   },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '/src'),
     },
   }
-}));
+});
