2db7ee9554760b41d8ca7fd9b59dbc700a3e62f0
Manthee1 <dinja.martin@gmail.com>
2025-05-09 22:39:00 +0200

    fix: Comment out base configuration in Vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '',
+  // base: '',
   plugins: [vue({
     include: [/\.vue$/, /\.md$/],
   }), Markdown()],
